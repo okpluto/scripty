d8a13ddb9b194ee5653013a90cb52d327389de96
annasophie <annasophie.nagel@gmail.com>
2016-10-21 19:20:10 -0400

    Added login route to navigator Updated default route to login/signup Added touchable buttons Enabled navigation to homepage through login

--- index.ios.js
+++ index.ios.js
@@ -18,25 +18,28 @@ import {
 import Header from './src/components/header';
 import LessonTitleCard from './src/components/lessonTitleCard';
 import LessonTitleCardList from './src/components/LessonTitleCardList';
+import Login from './src/components/login';
 
 
 
 class scripty extends Component {
-  
+
   renderScene(route, navigator) {
     const { name } = route;
     if (name === 'Home') {
-      return <LessonTitleCardList />
+      return <LessonTitleCardList navigator={navigator} />
+    } else if (name === 'Login') {
+      return <Login navigator={navigator}/>
     }
   }
 
   render() {
     return (
-      <Navigator 
-      style={{ backgroundColor: '#F0F5F9', }} 
+      <Navigator
+      style={{ backgroundColor: '#F0F5F9', }}
       navigationBar={<Header />}
-      initialRoute={{ name:'Home' }} 
-      renderScene={this.renderScene} 
+      initialRoute={{ name:'Login' }}
+      renderScene={this.renderScene}
       />
     )
   }
