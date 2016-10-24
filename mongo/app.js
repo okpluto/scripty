b2a2fa56464079c343d633e4d15e2fdedf32f30b
Emmanuel Vo <emmanuelvo777@gmail.com>
2016-10-24 15:15:32 -0700

    Build GET route

---
+++ mongo/app.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+var mongoose = require("mongoose");
+//var bodyParser = require("bodyParser");
+mongoose.connect("mongodb://localhost:8000/test");
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+});
+
+var Problems = require("./models/problems");
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.get('/api/test', function(req, res) {
+  Problems.find(function(err, problems) {
+    if(err) console.log("Error: ", err);
+    res.json(problems);
+  });
+});
+
+
+// app.post("/config", function(req, res) {
+//   db.config.insert(req.body, function(err, doc) {
+//     res.json(doc);
+//   });
+// });
+
+// app.get('/config/:id', function(req, res) {
+//   var id = req.params.id;
+//   db.config.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+//     res.json(doc);
+//   });
+// });
+
+app.listen(process.env.PORT || 8000);
--- mongo/models/problems.js
+++ mongo/models/problems.js
@@ -1,5 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect("/config.js");
+//mongoose.connect("mongodb://localhost:test");
+
 
 var problemSchema = mongoose.Schema({
   text: String,
@@ -7,6 +9,13 @@ var problemSchema = mongoose.Schema({
   answer: String
 });
 
+
 var Problems = mongoose.model("Problems", problemSchema);
+
+var helloWorld = new Problems({
+  text: " How would you print out 'Hello World'? ",
+  choices: ["print('Hello World')", "show('Hello World')", "console.log('Hello World')"],
+  answer: "The answer was: console.log('Hello World')."
+});
 
 module.exports = Problems;
