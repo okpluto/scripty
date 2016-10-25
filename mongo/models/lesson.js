32d8e8018e43691ce633ba4f151d5db29b2383ff
Emmanuel Vo <emmanuelvo777@gmail.com>
2016-10-25 15:34:04 -0700

    Resolved merge conflict

--- mongo/models/lesson.js
+++ mongo/models/lesson.js
@@ -9,21 +9,5 @@ var lessonSchema = mongoose.Schema({
 
 var Lesson = mongoose.model('Lesson', lessonSchema);
 
-<<<<<<< HEAD:mongo/models/lessons.js
-var lesson1 = new Lessons({
-  title: "Variable",
-  description: "In this lesson, you will be learning about variables"
-});
 
-lesson1.save(function(err, data) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-});
-
-module.exports = Lessons;
-=======
 module.exports = Lesson;
->>>>>>> 55c45c77974f59f4cc30abe48efb3ae3e9fc437b:mongo/models/lesson.js
--- mongo/models/problem.js
+++ mongo/models/problem.js
@@ -11,27 +11,6 @@ var problemSchema = mongoose.Schema({
   lesson: {type: Number, ref: 'lesson'}
 });
 
-<<<<<<< HEAD:mongo/models/problems.js
-
-var Problems = mongoose.model('Problems', problemSchema);
-
-var helloWorld = new Problems({
-  text: " How would you print out 'Hello World'? ",
-  choices: ["print('Hello World')", "show('Hello World')", "console.log('Hello World')"],
-  answer: "The answer was: console.log('Hello World')."
-});
-
-helloWorld.save(function(err, data) {
-  if(err) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-})
-
-module.exports = Problems;
-=======
 var Problem = mongoose.model('Problem', problemSchema);
 
 module.exports = Problem;
->>>>>>> 55c45c77974f59f4cc30abe48efb3ae3e9fc437b:mongo/models/problem.js
