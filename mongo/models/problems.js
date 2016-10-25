1103d6e17891c74d9979fd2f468e7efaecf1c8ef
Emmanuel Vo <emmanuelvo777@gmail.com>
2016-10-24 17:29:02 -0700

    Fixing merge issue. Added the save method.

--- mongo/models/problems.js
+++ mongo/models/problems.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
-<<<<<<< HEAD
+
 //mongoose.connect("/config.js");
 //mongoose.connect("mongodb://localhost:test");
-
-=======
->>>>>>> bf396f80118280d4efb0a7e950599e4f044b3c8b
 
 var problemSchema = mongoose.Schema({
   order: {type: Number, default: 0},
@@ -14,17 +11,21 @@ var problemSchema = mongoose.Schema({
   lesson: {type: Number, ref: 'lesson'}
 });
 
-<<<<<<< HEAD
 
-var Problems = mongoose.model("Problems", problemSchema);
-=======
 var Problems = mongoose.model('Problems', problemSchema);
->>>>>>> bf396f80118280d4efb0a7e950599e4f044b3c8b
 
 var helloWorld = new Problems({
   text: " How would you print out 'Hello World'? ",
   choices: ["print('Hello World')", "show('Hello World')", "console.log('Hello World')"],
   answer: "The answer was: console.log('Hello World')."
 });
+
+helloWorld.save(function(err, data) {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log(err);
+  }
+})
 
 module.exports = Problems;
