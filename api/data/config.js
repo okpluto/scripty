326d88e5fbd3327d06356603eb1b37c70220d685
fuzzwizard <momojak@gmail.com>
2016-10-26 22:34:53 -0700

    Add additional reporting.

--- api/data/config.js
+++ api/data/config.js
@@ -1,12 +1,15 @@
 var mongoose = require('mongoose');
+var log = require('../helpers/log');
 
 mongoose.connect('mongodb://localhost/scripty');
 
 var connection = mongoose.connection;
 
 connection.on('error',
-  console.error.bind(console, 'Error connecting to MongoDB:'));
-connection.once('open', function() {});
+  log.error.bind(null, 'Error connecting to MongoDB:'));
+connection.once('open', function() {
+  log.success('Connected to MongoDB.');
+});
 
 module.exports = connection;
 
