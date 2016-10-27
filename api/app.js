77f17196259a917f92bdc146d4b92bfec8ae0a94
fuzzwizard <momojak@gmail.com>
2016-10-27 00:31:17 -0700

    Update to es6 standards. Target new route handlers.

--- api/app.js
+++ api/app.js
@@ -1,17 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var log = require('./helpers/log');
+const handlers = require('./util/route-handlers');
+const log = require('./helpers/log');
+const db = require('./data/config');
 
-var db = require('./data/config');
-
-var handlers = require('./util/route-handlers');
+const app = express();
 
 app.use(bodyParser.json());
 
 // Apply headers
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -19,23 +18,23 @@ app.use(function(req, res, next) {
 });
 
 // Log out requests for debug
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   log.info(`Request recieved from ${req.url} with method ${req.method}.`);
   next();
 });
 
+/*
+Add update, delete for Users/Lesssons.
+*/
+
 app.get('/api/lessons', handlers.getAllLessons);
-
 app.get('/api/lessons/:id', handlers.getLessonById);
-
 app.post('/api/lessons', handlers.createLesson);
 
 app.get('/api/users', handlers.getUsers);
 app.get('/api/users/:id', handlers.getUserById);
-// app.get('/api/users/:name', handlers.getUserByName);
-
 app.post('/api/users', handlers.createUser);
 
-app.listen(process.env.PORT || 3011, function() {
-  log(`Listening on port ${process.env.PORT || 3011}`);
+app.listen(process.env.PORT || 3011, () => {
+  log.info(`Listening on port ${process.env.PORT || 3011}`);
 });
