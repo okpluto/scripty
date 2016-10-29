ebb8e887f7d152ca29836ed66d59cfd298ceb2fe
fuzzwizard <momojak@gmail.com>
2016-10-28 22:08:35 -0700

    Add documentation of data types.

--- api/data/models/content.js
+++ api/data/models/content.js
@@ -1,5 +1,29 @@
 const mongoose = require('mongoose');
 
+/* Data type: Content
+ *
+ * Covers the range of content held within lessons: questions, readings, slides,
+ * videos, recipes, etc.
+ *
+ * This datatype is a starting point, and the only fields that are considered
+ * *required* are `order` and `lessonId`. Afterall, content is nothing without
+ * a lesson. This could change with cirriculum restructuring but the data type
+ * is purposefully left flexible to reflect this. Praise be to mongo for
+ * allowing this lack of discipline.
+ *
+ * @order: <Number>
+ *  Represents the order the content appears in the given lesson.
+ * @type: <String> ('reading', 'question' currently supported)
+ *  The `type` of content.
+ * @choices: [<String>]
+ *  If the content is a question or otherwise requires confirmation from the
+ *  user, this represents the choices the user can make.
+ * @answer: <String>
+ *  The choice from the `choices` array which represents the correct answer.
+ * @lessonId: <ObjectId>
+ *  The reference id of the lesson which holds the content.
+ **/
+
 const contentSchema = mongoose.Schema({
   order: {type: Number, default: 0},
   type: String,
--- api/data/models/user.js
+++ api/data/models/user.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose');
 
-/*
-  TODO: Achievement and progress info should be included in the schema.
-*/
+/* Data type: User
+ *
+ * Not currently developed. May need to accomidate lesson progress and achieve-
+ * ments. Not to mention password encryption.
+ *
+ * @name: <String>
+ *  The user's username.
+ * @email: <String>
+ *  The user's email.
+ * @password: <String>
+ *  Believe it or not: it's the user's password.
+ **/
+
 const userSchema = mongoose.Schema({
   name: String,
   email: String,
