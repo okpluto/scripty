bf1752bf20c4f2f4c0ff651cc2cac5f5d29d52a8
Ivey Topaz <iveytopaz@gmail.com>
2016-11-03 20:46:08 -0400

    adding lesson results to profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Text, View, Dimensions, ScrollView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import { localIp } from '../../config/ip.js'
+
+const days = 'Su M T W Th F Sa'.split(' ')
 
 class Profile extends Component {
 
@@ -12,34 +15,72 @@ class Profile extends Component {
     this.getUserInfo()
   }
 
-
   getUserInfo() {
     //Using get all users to test dummy user
     //Need to get user by ID once signin
     //and save users to DB is implemented
-    let url=`http://10.226.56.128:3011/api/users`
+    let url=`http://${localIp}:3011/api/users`
     fetch(url)
     .then(user => {
       return user.json()
     })
     .then(user => {
-      this.setState({user: user[0]})
+      this.setState({user: user[0]});
+      this.setDayHighlight();
     })
   }
 
+  setDayHighlight(day) {
+    let highlights = this.state.user.streak;
+    let dayNum = days.indexOf(day);
+    return highlights.indexOf(dayNum) !== -1;
+  }
+
+  getScoreStyle(score, style) {
+    styleCopy = {}
+    for (var key in style) {
+      styleCopy[key] = style[key]
+    }
+    let scoreArray = score.split('/')
+    let percent = Number(scoreArray[0]) / Number(scoreArray[1])
+    let color = '#2aa009'
+    if (percent < 0.65) {
+      color = '#a80f0f'
+    } else if (percent < 0.85) {
+      color = '#f2d204'
+    }
+    styleCopy.borderColor = color;
+    return styleCopy
+  }
+
   render() {
-    const { viewStyle, textStyle, profileStyle, nameTextStyle } = styles;
+    const { viewStyle, textStyle, profileStyle, nameTextStyle, dayStyle, circleActiveStyle, circleInactiveStyle, lessonStyle, scrollStyle, scoreCircle, lessonContainerStyle } = styles;
     if (this.state.user) {
       console.log(this.state.user)
       return (
         <View style={profileStyle}>
           <Text style={nameTextStyle}>
-          <Icon name="user-circle" size={50} />
+          <Icon name='user-circle' size={50} />
            {' ' + this.state.user.name}
           </Text>
           <Text style={textStyle}>
-            Current Streak: {this.state.user.streak} Days
+            Current Streak: {this.state.user.streak.length} Days
           </Text>
+          <View style={dayStyle}>
+          {days.map(day => (
+          <Text key={day} style={(this.setDayHighlight(day) ? circleActiveStyle : circleInactiveStyle)}>{day}</Text>
+          ))}
+          </View>
+          <ScrollView contentContainerStyle={scrollStyle}>
+            {this.state.user.lessons.map(lesson => (
+              <View style={lessonContainerStyle}>
+                <Text key={lesson.lessonId} style={lessonStyle}>
+                  {lesson.title}
+                </Text>
+                <Text style={this.getScoreStyle(lesson.score, scoreCircle)}>{lesson.score}</Text>
+              </View>
+            ))}
+          </ScrollView>
         </View>
       )
     } else {
@@ -66,7 +107,7 @@ const styles = {
     height: Dimensions.get("window").height,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'white',
+    backgroundColor: 'white'
     // flexWrap: 'wrap',
     // flexDirection: 'row',
   },
@@ -74,13 +115,74 @@ const styles = {
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'center',
-
     height: 60,
     width: Dimensions.get("window").width - 40,
     marginTop: 20,
     borderRadius: 5,
     position: 'relative',
 
+  },
+  scrollStyle: {
+    backgroundColor: grey,
+    width: Dimensions.get("window").width - 40,
+    height: 300,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: 'gainsboro',
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5
+  },
+  dayStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+    marginBottom: 10
+  },
+  lessonStyle: {
+    fontSize: 33
+  },
+  circleActiveStyle: {
+    backgroundColor: coral,
+    borderRadius: 100/2,
+    width: 40,
+    height: 40,
+    marginRight: 5,
+    textAlign: 'center',
+    paddingTop: 5,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: grey
+  },
+  circleInactiveStyle: {
+    backgroundColor: 'grey',
+    borderRadius: 50,
+    width: 40,
+    height: 40,
+    marginRight: 5,
+    textAlign: 'center',
+    paddingTop: 5,
+    fontSize: 20,
+    fontWeight: 'bold'
+  },
+  lessonContainerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start'
+  },
+  scoreCircle: {
+    borderRadius: 100/2,
+    width: 60,
+    height: 60,
+    marginRight: 5,
+    textAlign: 'center',
+    paddingTop: 9,
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: grey,
+    borderWidth: 4,
+    backgroundColor: 'grey'
   },
   pinkCardStyle: {
     backgroundColor: coral,
