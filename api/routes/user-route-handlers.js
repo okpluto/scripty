e6b8d19e5d928da9f24f0fd42b8d40bed52a2666
Ivey Topaz <iveytopaz@gmail.com>
2016-11-06 22:58:47 -0500

    adding update user by id route

--- api/routes/user-route-handlers.js
+++ api/routes/user-route-handlers.js
@@ -26,7 +26,7 @@ exports.getUserById = (req, res) => {
   User.findById(id)
   .then(user => {
     console.log(user)
-    res.status(200).json({createdLessons: user.createdLessons})
+    res.status(200).json(user)
   })
 };
 
@@ -75,7 +75,38 @@ exports.createUser = (req, res) => {
 
 exports.updateUserById = (req, res) => {
   const id = req.params.id;
-  //TODO(Mitch): Fill me in!
+  log.amazing('message received', req.body)
+  let day = new Date(req.body.lastLessonDate).getDay().toString()
+  if (req.body.streak === 'reset') {
+    User.update({ _id: req.params.id}, { $set: {
+      streak: [day],
+      lastLessonDate: req.body.lastLessonDate
+    } })
+  } else if (req.body.streak === 'same') {
+    User.update({ _id: req.params.id}, { $set: {
+      lastLessonDate: req.body.lastLessonDate
+    } })
+  } else if (req.body.streak === 'add') {
+    log.warning('should push to streak', day)
+    User.update({ _id: req.params.id }, {
+      $push: {
+        streak: day
+      },
+      $set: {
+        lastLessonDate: req.body.lastLessonDate
+      } })
+  }
+  User.update({ _id: req.params.id }, { $push: {
+    lessons: {
+      lessonId: req.body.lessonId,
+      title: req.body.title,
+      score: req.body.score
+    } } }, (err, user) => {
+    if (err) {
+      log.error(err)
+    }
+    log.amazing(user)
+  })
 };
 
 exports.deleteUserById = (req, res) => {
