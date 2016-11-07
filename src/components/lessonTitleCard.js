0299e188a518bc5911ffee955e341c474fef6b32
Ivey Topaz <iveytopaz@gmail.com>
2016-11-06 23:02:04 -0500

    displaying difficulty rating and user rating

--- src/components/lessonTitleCard.js
+++ src/components/lessonTitleCard.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
 import { Text, View, Dimensions, TouchableHighlight } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+const difficulty = ['','Basics', 'Easy', 'Med', 'Hard', 'Insane']
+const blues = ['', '#00C2FC', '#009FCC', '#007799', '#004F66', '#002732']
 
+const LessonTitleCard = ({ lessonTitle, navigator, lessonId, lessonDifficulty, lessonRating }) => {
+  const { buttonStyle, viewStyle, textStyle, circleStyle, rightTextStyle, titleStyle, starStyle } = styles;
 
+  const getCircleColor = (style) => {
+    styleCopy = {...style};
+    styleCopy.backgroundColor = blues[lessonDifficulty];
+    return styleCopy
+  };
 
-const LessonTitleCard = ({ lessonTitle, navigator, lessonId }) => {
-  const { buttonStyle, viewStyle, textStyle, circleStyle } = styles;
+  const renderStars = () => {
+    if (!lessonRating) {
+      return <Text>No ratings yet</Text>
+    }
+    let numStars = Math.floor(lessonRating);
+    let remainder = lessonRating % 1;
+    let halfStar = false;
+    if (remainder > .75) {
+      numStars++;
+    } else if (remainder > .25) {
+      halfStar = true;
+    }
+    let star = <Icon name='star' size={15} style={starStyle}/>
+    let starArray = []
+    for (var i = 0; i < numStars; i++) {
+      starArray.push(star)
+    }
+    if (halfStar) {
+      starArray.push(<Icon name='star-half' size={15} style={starStyle}/>)
+    }
+    return starArray
+  }
 
   const navigate = (routeName, id, lessonTitle) => {
     navigator.push({
@@ -20,8 +50,14 @@ const LessonTitleCard = ({ lessonTitle, navigator, lessonId }) => {
   return (
     <TouchableHighlight onPress={navigate.bind(this, 'lesson', lessonId, lessonTitle)} underlayColor={grey} style={buttonStyle}>
       <View style={viewStyle}>
-        <View style={circleStyle}></View>
-        <Text style={textStyle}>{lessonTitle}</Text>
+        <View style={titleStyle}>
+          <View style={getCircleColor(circleStyle)}></View>
+            <View>
+            <Text style={textStyle}>{lessonTitle}</Text>
+              <View style={titleStyle}>{renderStars()}</View>
+            </View>
+        </View>
+        <Text style={rightTextStyle}>{difficulty[lessonDifficulty]}</Text>
       </View>
     </TouchableHighlight>
   )
@@ -33,10 +69,8 @@ const grey = '#FAFAFA'
 const styles = {
   buttonStyle: {
     backgroundColor: 'white',
-
     alignItems: 'center',
     flexDirection: 'row',
-
     height: 75,
     width: Dimensions.get("window").width - 20,
     marginTop: 0,
@@ -44,19 +78,32 @@ const styles = {
     borderBottomWidth: 0.5,
   },
   viewStyle: {
+    flex: 1,
     alignItems: 'center',
     flexDirection: 'row',
-    height: 120,
+    justifyContent: 'space-between',
+  },
+  titleStyle: {
+    flex: 1,
+    height: 38,
+    flexDirection: 'row',
+  },
+  starStyle: {
+    color: '#f2d204'
   },
   textStyle: {
     color: '#1c1c1c',
     fontSize: 17,
   },
+  rightTextStyle: {
+    color: '#1c1c1c',
+    fontSize: 17,
+    marginRight: 10
+  },
   circleStyle: {
     borderRadius: 100,
     height: 30,
     width: 30,
-    backgroundColor: '#4DD8F9',
     marginLeft: 20,
     marginRight: 20,
   }
